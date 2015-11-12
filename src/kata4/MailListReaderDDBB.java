4ed4bda42072b2f937177325e25fb584882a74dc
usuario <usuario@10.22.143.14>
2015-11-12 10:04:56 +0000

    copia

---
+++ src/kata4/MailListReaderDDBB.java
@@ -0,0 +1,45 @@
+package kata4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MailListReaderDDBB {
+
+    public MailListReaderDDBB() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+            //Class.forName("oracle.jdbc.driver.OracleDriver");
+            //Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
+
+            System.out.println("Opened database successfully");
+
+            Statement stmt = c.createStatement();
+
+            String fileName = "emailsfilev1.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+            String mail;
+            while ((mail = reader.readLine()) != null) {
+                String query = "INSERT INTO MAILS (MAIL) VALUES ('" + mail + "')";
+                stmt.executeUpdate(query);
+            }
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PEOPLE");
+            while (rs.next()) {
+                System.out.println("ID = " + rs.getInt("ID"));
+                System.out.println("NAME = " + rs.getString("NAME"));
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
